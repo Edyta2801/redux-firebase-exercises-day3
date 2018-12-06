91604f8482b3fb504353a135d066976ff4324672
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-06 15:03:43 +0100

    loadTextFromDbAsyncAction added

--- src/state/userData.js
+++ src/state/userData.js
@@ -3,31 +3,49 @@ import { database } from '../firebaseConfig'
 const TEXT_CHANGE = 'userData/TEXT_CHANGE'
 
 export const textChangeAction = newTextValue => ({
-  type: TEXT_CHANGE,
-  newTextValue
+    type: TEXT_CHANGE,
+    newTextValue
 })
 
 export const saveTextToDbAsyncAction = () => (dispatch, getState) => {
-  const text = getState().userData.text
-  const uuid = getState().auth.user.uid
+    const {
+        userData: { text },
+        auth: { user: { uid: uuid } }
+    } = getState()
+    // above is the same that below only using destructurization
+    // const text = getState().userData.text
+    // const uuid = getState().auth.user.uid
 
-  database.ref(`jfddl6/users/${uuid}`).set({
-    text
-  })
+    database.ref(`jfddl6/users/${uuid}`).set({
+        text
+    })
+}
+
+export const loadTextFromDbAsyncAction = () => (dispatch, getState) => {
+    const uuid = getState().auth.user.uid
+
+    database.ref(`jfddl6/users/${uuid}/text`).once(
+        'value',
+        snapshot => {
+            dispatch(
+                textChangeAction(snapshot.val() || '')
+            )
+        }
+    )
 }
 
 const INITIAL_STATE = {
-  text: ''
+    text: ''
 }
 
 export default (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case TEXT_CHANGE:
-      return {
-        ...state,
-        text: action.newTextValue
-      }
-    default:
-      return state
-  }
+    switch (action.type) {
+        case TEXT_CHANGE:
+            return {
+                ...state,
+                text: action.newTextValue
+            }
+        default:
+            return state
+    }
 }
