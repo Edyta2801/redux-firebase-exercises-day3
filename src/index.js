a4805689c68580880d24cfeeb982b8036d98f05a
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-06 10:12:55 +0100

    instal redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux'
 
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
-
+import { store } from './store'
 
 ReactDOM.render(
-    <MuiThemeProvider>
-        <App />
-    </MuiThemeProvider>,
+    <Provider
+        store={store}
+    >
+
+        <MuiThemeProvider>
+            <App />
+        </MuiThemeProvider>
+    </Provider>,
     document.getElementById('root')
-    );
+);
 
