6c05df2c25376956ef0c484594479286a1579d2f
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-06 14:21:38 +0100

    create duck userData

---
+++ src/state/userData.js
@@ -0,0 +1,31 @@
+
+
+
+const TEXT_CHANGE = 'userData/TEXT_CHANGE'
+
+
+
+
+const INITIAL_STATE = {
+    text: ''
+}
+
+export const textChangeAction = newTextValue => ({
+    type: TEXT_CHANGE,
+    newTextValue
+})
+
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case TEXT_CHANGE:
+            return {
+                ...state,
+                text: action.newTextValue
+            }
+
+        default:
+            return state
+    }
+}
+
